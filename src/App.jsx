4b68e5cac5ac0fdf6b62ff78890d4bb82a5bdb31
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-06-29 00:58:01 -0500

    fixed clickable areas

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,35 @@ function App() {
                 Frontend Developer based in Mexico
             </p>
             <div className="flex flex-col max-w-5xl mx-auto">
-                <button className="bg-yellow-nb p-4 my-4 flex flex-row justify-center items-center shadow-custom-s hover:bg-green-nb">
+                <button
+                    className="bg-yellow-nb p-4 my-4 flex flex-row justify-center items-center shadow-custom-s hover:bg-green-nb"
+                    onClick={(e) => {
+                        e.preventDefault()
+                        window.open('https://twitter.com/soysarcasme')
+                    }}
+                >
                     <FaTwitter className="mr-3" />
-                    <a href="https://twitter.com/soysarcasme" target="_blank">
-                        Twitter
-                    </a>
+                    <p>Twitter</p>
                 </button>
-                <button className="bg-red-nb p-4 my-4 flex flex-row justify-center items-center shadow-custom-s hover:bg-green-nb">
+                <button
+                    className="bg-red-nb p-4 my-4 flex flex-row justify-center items-center shadow-custom-s hover:bg-green-nb"
+                    onClick={(e) => {
+                        e.preventDefault()
+                        window.open('https://github.com/dreyfus92')
+                    }}
+                >
                     <FaGithub className="mr-3" />
-                    <a href="https://github.com/dreyfus92" target="_blank">
-                        Github
-                    </a>
+                    <p>Github</p>
                 </button>
-                <button className="bg-pink-nb p-4 my-4 flex flex-row justify-center items-center shadow-custom-s hover:bg-green-nb">
+                <button
+                    className="bg-pink-nb p-4 my-4 flex flex-row justify-center items-center shadow-custom-s hover:bg-green-nb"
+                    onClick={(e) => {
+                        e.preventDefault()
+                        window.open('https://www.instagram.com/sarcasme92')
+                    }}
+                >
                     <FaInstagram className="mr-3" />
-                    <a
-                        href="https://www.instagram.com/sarcasme92/"
-                        target="_blank"
-                    >
-                        Instagram
-                    </a>
+                    <p>Instagram</p>
                 </button>
             </div>
         </div>
